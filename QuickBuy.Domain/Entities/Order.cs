c09ec1b51071ebb681a089cb0c3bcc57fbcf68ac
Nathan Ribeiro <nathanseribeiro@hotmail.com>
2019-12-11 22:33:43 -0300

    Entities creation

---
+++ QuickBuy.Domain/Entities/Order.cs
@@ -0,0 +1,28 @@
+﻿using QuickBuy.Domain.Valueables;
+using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Domain.Entities
+{
+    class Order //Pedido
+    {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public string ZipCode { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string CompleteAddress { get; set; }
+        public int AddressNumber { get; set; }
+
+        public int PaymentOptionId { get; set; }
+        public PaymentOption PaymentOption { get; set; }
+
+        /// <summary>
+        /// An order must have at least one OrderItem
+        /// or many OrderItems
+        /// </summary>
+        public ICollection<OrderItem> OrderItens { get; set; }
+    }
+}
---
+++ QuickBuy.Domain/Enumerables/PaymentOptionTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickBuy.Domain.Enumerables
+{
+    public enum PaymentOptionTypeEnum
+    {
+        NotDefined = 0,
+        PaymentSlip = 1, //Boleto
+        CreditCard = 2,
+        Deposit = 3
+    }
+}
